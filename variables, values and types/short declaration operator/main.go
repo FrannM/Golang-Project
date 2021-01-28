f765a14ff6fe008fe886cbf4e24ef67cd96dede0
Frann <fmaurizzio@gmail.com>
2021-01-28 04:51:45 -0300

    bla bla

---
+++ variables, values and types/short declaration operator/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+var y = 43
+
+func main() {
+	// short declaration operator
+	// DECLARE a variable and ASSIGN a VALUE (of a certain TYPE)
+	x := 42
+	fmt.Println(x)
+
+	fmt.Println(y)
+
+	foo()
+}
+
+func foo() {
+	fmt.Println("again:", y)
+}
